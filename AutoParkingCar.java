d1e945396be2a29e4b5ff9d6c06958692ed4c9f9
joelpalsson <joelpalsson1998@hotmail.com>
2023-09-12 10:05:21 +0200

    a

---
+++ AutoParkingCar.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class AutoParkingCar {
+    private double[] ultraSoundSensorOne;
+    private double[] ultraSoundSensorTwo;
+
+    public AutoParkingCar(double[] sens1, double[] sens2) {
+        //Car startValues
+        double carPos = 0;
+        boolean isParked = false;
+        ultraSoundSensorOne = sens1;
+        ultraSoundSensorTwo = sens2;
+    }
+
+    void MoveForward(){}
+
+    void MoveBackwards(){}
+
+    double isEmpty(){
+        double Au;
+        isNoisy(car)
+
+        
+        return distance;
+
+    }
+
+    boolean isNoisy(double[] sensorData){
+
+    }
+
+    void Park(){}
+
+    void UnPark(){}
+
+    void WhereIs(){}
+
+
+}
---
+++ AutoParkingCarTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+import static org.junit.Assert.assertEquals;
+
+class AutoParkingCarTest {
+    AutoParkingCar car;
+
+    @BeforeEach
+     void setupCar() {
+        car = new AutoParkingCar();
+    }
+
+
+    @Test
+    void didCarMoveForwardTest() {
+        assertEquals();
+
+    }
+
+    @Test
+    void didCarMoveBackwardsTest() {
+    }
+
+    @Test
+    void isSensorEmptyTest() {
+
+    }
+
+    @Test
+    void parkCarTest() {
+    }
+
+    @Test
+    void unParkTest() {
+    }
+
+    @Test
+    void whereIsCarTest() {
+    }
+}
