30817218bb53e6df2097c65b482b5d1b5364c4f6
simba <johanelfing@gmail.com>
2023-09-14 12:57:32 +0200

    k

--- AutoParkingCar.java
+++ AutoParkingCar.java
@@ -1,9 +1,15 @@
+import java.util.Random;
+
 public class AutoParkingCar {
     public context con;
     private int[] ultraSoundSensorOne;
     private int[] ultraSoundSensorTwo;
     private int oneMeter = 100;
 
+    private boolean parkingSpot = false;
+
+    public int counter = 0;
+    private int[] parkingPlace;
     private int carPos = 0;
     private int endOfTheStreet = 50000;
     private int startOfStreet = 0;
@@ -41,32 +47,59 @@ public class AutoParkingCar {
         con.situation = false;
         ultraSoundSensorOne = sens1;
         ultraSoundSensorTwo = sens2;
+        parkingPlace = generateRandomParking(500, 5);
 
     }
 
+    public int[] generateRandomParking(int length, int groupSize) {
+        int[] array = new int[length];
+        Random random = new Random();
+
+        int currentIndex = 0;
+
+        while (currentIndex < length) {
+            int groupStartIndex = currentIndex;
+            int groupEndIndex = Math.min(currentIndex + groupSize, length);
+
+            // Generate a random number (0 or 1) for each element in the current group
+            int randomValue = random.nextInt(2);
+
+            // Fill the current group with the same random value
+            for (int i = groupStartIndex; i < groupEndIndex; i++) {
+                array[i] = randomValue;
+            }
+
+            // Move to the next group
+            currentIndex = groupEndIndex;
+        }
+        return array;
+    }
+
     /**
-     Description:
-     MoveForward: : This method moves the car 1 meter forward, queries the two sensors through the isEmpty method
-     described below and returns a data structure that contains the current position of the car, and the situation
-     of the detected parking places up to now. The car cannot be moved forward beyond the end of the street.
-
-     Pre-condition: Car is not at the end of the street
-
-     Post-condition: Car has moved 1 meter forward and has returned a data structure containing the current position of
-     the car and the situation
-
-     Test-cases:
-     MoveForwardFromStartTest
-     MoveForwardFromEndOfStreet
-     ...?
+     * Description:
+     * MoveForward: : This method moves the car 1 meter forward, queries the two sensors through the isEmpty method
+     * described below and returns a data structure that contains the current position of the car, and the situation
+     * of the detected parking places up to now. The car cannot be moved forward beyond the end of the street.
+     * <p>
+     * Pre-condition: Car is not at the end of the street
+     * <p>
+     * Post-condition: Car has moved 1 meter forward and has returned a data structure containing the current position of
+     * the car and the situation
+     * <p>
+     * Test-cases:
+     * MoveForwardFromStartTest
+     * MoveForwardFromEndOfStreet
+     * ...?
      */
     public context MoveForward() {
         if (con.position >= endOfTheStreet) {
             //start over from start?
+
             con.position = startOfStreet;
             return null;
         } else {
             con.position = con.position + oneMeter;
+            counter++;
             isEmpty();
         }
 
@@ -76,50 +109,52 @@ public class AutoParkingCar {
 
 
     /**
-     Description:
-     MoveBackward: The same as above; only it moves the car 1 meter backwards. The car cannot be moved behind if it is
-     already at the beginning of the street.
-
-     Pre-condition: Car is not at the beginning of the street
-
-     Post-condition: Car has moved 1 meter backward and has returned a data structure containing the current position of
-     the car and the situation.
-
-     Test-cases:
-     MoveBackwardsFromStartTest
-     MoveBackwardsFromEndOfStreet
-     ...?
+     * Description:
+     * MoveBackward: The same as above; only it moves the car 1 meter backwards. The car cannot be moved behind if it is
+     * already at the beginning of the street.
+     * <p>
+     * Pre-condition: Car is not at the beginning of the street
+     * <p>
+     * Post-condition: Car has moved 1 meter backward and has returned a data structure containing the current position of
+     * the car and the situation.
+     * <p>
+     * Test-cases:
+     * MoveBackwardsFromStartTest
+     * MoveBackwardsFromEndOfStreet
+     * ...?
      */
     public context MoveBackwards() {
-        if (con.position <= startOfStreet) {
-            System.out.println("Car went to far");
-        }
 
         con.position = con.position - oneMeter;
 
+        if (con.position <= startOfStreet) {
+            con.position = 0;
+        }
+
+        counter--;
         return con;
     }
 
     /**
-     Description:
-     isNoisy: This method takes in an array of integers and creates a 5 long array based on the cars position, if any
-     value in the array exceeds 200 or is below 0 returns true. We then iterate through the array and find the smallest
-     and biggest values and if the difference is bigger than 120 then return true. Otherwise, return false
-
-     Pre-condition: Sensor data has at least 1 value
-
-     Post-condition: returns the correct boolean value
-
-     Test-cases:
-     sensorDataNotNoisyTest
-     sensorDataNoisyTest
-     sensorDataNegativeTest
-     sensorDataTooBigTest
-     ...?
+     * Description:
+     * isNoisy: This method takes in an array of integers and creates a 5 long array based on the cars position, if any
+     * value in the array exceeds 200 or is below 0 returns true. We then iterate through the array and find the smallest
+     * and biggest values and if the difference is bigger than 120 then return true. Otherwise, return false
+     * <p>
+     * Pre-condition: Sensor data has at least 1 value
+     * <p>
+     * Post-condition: returns the correct boolean value
+     * <p>
+     * Test-cases:
+     * sensorDataNotNoisyTest
+     * sensorDataNoisyTest
+     * sensorDataNegativeTest
+     * sensorDataTooBigTest
+     * ...?
      */
     boolean isNoisy(int[] sensorData) {
         //illegal startvalue
-        if(sensorData[0]>200||sensorData[0]<0) return true;
+        if (sensorData[0] > 200 || sensorData[0] < 0) return true;
 
         int max = sensorData[0];
         int min = sensorData[0];
@@ -127,13 +162,13 @@ public class AutoParkingCar {
 
         for (int i = 1; i < sensorData.length; i++) {
 
-            if(sensorData[i]<0||sensorData[i]>200) return true;
+            if (sensorData[i] < 0 || sensorData[i] > 200) return true;
 
-            if(sensorData[i] > max){
+            if (sensorData[i] > max) {
 
                 max = sensorData[i];
 
-            }else if(sensorData[i] < min) min = sensorData[i];
+            } else if (sensorData[i] < min) min = sensorData[i];
 
         }
         //If the difference between the largest and smalles data exceeds 120 the data is noisy
@@ -142,22 +177,22 @@ public class AutoParkingCar {
     }
 
     /**
-     Description:
-     isEmpty: This method queries the two ultrasound sensors at least 5 times and filters the noise in their results
-     and returns the distance to the nearest object in the right hand side. If one sensor is detected to continuously
-     return very noisy output, it should be completely disregarded. You can use averaging or any other statistical
-     method to filter the noise from the signals received from the ultrasound sensors.
-
-     Pre-condition: Sensor data has at least 5 values
-
-     Post-condition: the returned distance is inbetween the values 0-200
-
-     Test-cases:
-     isEmptyWithTwoWorkingSensorsTest
-     isEmptyWithFirstSensorBrokenTest
-     isEmptyWithSecondSensorBrokenTest
-     isEmptyWithBothSensorsBrokenTest
-     ...?
+     * Description:
+     * isEmpty: This method queries the two ultrasound sensors at least 5 times and filters the noise in their results
+     * and returns the distance to the nearest object in the right hand side. If one sensor is detected to continuously
+     * return very noisy output, it should be completely disregarded. You can use averaging or any other statistical
+     * method to filter the noise from the signals received from the ultrasound sensors.
+     * <p>
+     * Pre-condition: Sensor data has at least 5 values
+     * <p>
+     * Post-condition: the returned distance is inbetween the values 0-200
+     * <p>
+     * Test-cases:
+     * isEmptyWithTwoWorkingSensorsTest
+     * isEmptyWithFirstSensorBrokenTest
+     * isEmptyWithSecondSensorBrokenTest
+     * isEmptyWithBothSensorsBrokenTest
+     * ...?
      */
     int isEmpty() {
         int distance = 0;
@@ -205,49 +240,58 @@ public class AutoParkingCar {
         for (int value : fiveSensValuesTwo) {
             distance += value;
         }
-        return distance / (fiveSensValuesOne.length+fiveSensValuesTwo.length); // average of 10 values (5 from each sensor)
+        return distance / (fiveSensValuesOne.length + fiveSensValuesTwo.length); // average of 10 values (5 from each sensor)
 
     }
 
 
     /**
-     Description:
-     Park: It moves the car to the beginning of the current 5 meter free stretch of parking place, if it is already
-     detected or moves the car forwards towards the end of the street until such a stretch is detected. Then it
-     performs a pre-programmed reverse parallel parking maneuver.
-
-     Pre-condition: There is an available parkingspot
-
-     Post-condition: The car should be parked
-
-     Test-cases:
-     parkWhenNoSpaceTest
-     parkWhenFirstSpaceIsFreeTest
-     ...
-     ...?
+     * Description:
+     * Park: It moves the car to the beginning of the current 5 meter free stretch of parking place, if it is already
+     * detected or moves the car forwards towards the end of the street until such a stretch is detected. Then it
+     * performs a pre-programmed reverse parallel parking maneuver.
+     * <p>
+     * Pre-condition: There is an available parkingspot
+     * <p>
+     * Post-condition: The car should be parked
+     * <p>
+     * Test-cases:
+     * parkWhenNoSpaceTest
+     * parkWhenFirstSpaceIsFreeTest
+     * ...
+     * ...?
      */
     public context Park() {
-        if (isEmpty() > 180) {
-            con.situation = true;
-            System.out.println("You parked your car");
+        for (int i = 0; i <= parkingPlace.length - 5; i++) {
+            if (parkingPlace[i] == 0 && parkingPlace[i + 1] == 0 && parkingPlace[i + 2] == 0 &&
+                    parkingPlace[i + 3] == 0 && parkingPlace[i + 4] == 0) {
+                MoveForward();
+                parkingSpot = true;
+                break;
+            }
         }
-        return con;
-    }
+            if (isEmpty() > 180 && parkingSpot) {
+                con.situation = true;
+                System.out.println("You parked your car");
+            }
+            return con;
+        }
+
 
 
     /**
-     Description:
-     UnPark: It moves the car forward (and to left) to front of the parking place, if it is parked.
-
-     Pre-condition: That we are not at the end of the street and that we are parked
-
-     Post-condition: The car should be unparked and moved forward
-
-     Test-cases:
-     UnparkWhenCarIsParkedTest
-     UnparkWhenCarisNotParkedTest
-     UnparkWhenAtEndOfStreetTest
-     ...?
+     * Description:
+     * UnPark: It moves the car forward (and to left) to front of the parking place, if it is parked.
+     * <p>
+     * Pre-condition: That we are not at the end of the street and that we are parked
+     * <p>
+     * Post-condition: The car should be unparked and moved forward
+     * <p>
+     * Test-cases:
+     * UnparkWhenCarIsParkedTest
+     * UnparkWhenCarisNotParkedTest
+     * UnparkWhenAtEndOfStreetTest
+     * ...?
      */
     public context UnPark() {
         con.situation = false;
@@ -256,24 +300,23 @@ public class AutoParkingCar {
     }
 
     /**
-     Description:
-     WhereIs: This method returns the current position of the car in the street as well as its situation
-     (whether it is parked or not).
-
-     Pre-condition: Car exists?
-
-     Post-condition: The position and situation is returned successfully
-
-     Test-cases:
-     CarIsParkedTest
-     CarisNotParkedTest
-     ...
-     ...?
+     * Description:
+     * WhereIs: This method returns the current position of the car in the street as well as its situation
+     * (whether it is parked or not).
+     * <p>
+     * Pre-condition: Car exists?
+     * <p>
+     * Post-condition: The position and situation is returned successfully
+     * <p>
+     * Test-cases:
+     * CarIsParkedTest
+     * CarisNotParkedTest
+     * ...
+     * ...?
      */
     public context WhereIs() {
         return con;
 
     }
-
 
 }
--- AutoParkingCarTest.java
+++ AutoParkingCarTest.java
@@ -1,161 +1,205 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Random;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AutoParkingCarTest {
     AutoParkingCar car;
-    private int[] dummySens1;
-    private int[] dummySens2;
+    private int[] dummyNoSens1;
+    private int[] dummyNoSens2;
+    private int[] parkingPlace;
+
+
+    AutoParkingCar yesParkCar;
+    private int[] dummyYesSens1;
+    private int[] dummyYesSens2;
 
 
     @BeforeEach
-    void setupCar() {
-        dummySens1 = new int[]{100, 110, 105, 108, 115};
-        dummySens2 = new int[]{95, 105, 98, 112, 100};
+        //Setting up two cars, one that has free parking and other one who hasn't at the start of the street, with sensors and start position and that it is not parked.
+    void setupCars() {
+        // Initialize dummy sensor data and car context for testing.
+        dummyNoSens1 = new int[]{100, 110, 105, 108, 115};
+        dummyNoSens2 = new int[]{95, 105, 98, 112, 100};
+        dummyYesSens1 = new int[]{200, 190, 180, 195, 185};
+        dummyYesSens2 = new int[]{180, 179, 193, 191, 199};
         AutoParkingCar.context dummyContext = new AutoParkingCar.context(0, false);
-        car = new AutoParkingCar(dummySens1, dummySens2, dummyContext);
+
+        car = new AutoParkingCar(dummyNoSens1, dummyNoSens2, dummyContext);
+        yesParkCar = new AutoParkingCar(dummyYesSens1, dummyYesSens2, dummyContext);
+        parkingPlace = car.generateRandomParking(500, 5);
+
+    }
+
+    @Test
+    void isCarInTheStartOfTheStreetTest() {
+        //Make sure that the car is in the start position
+        assertEquals(0, car.con.getPosition());
+        assertEquals(0, yesParkCar.con.getPosition());
+
+    }
+
+    @Test
+    void testGenerateRandomArrayForZeroRow() {
+        // Check if there is a row of (0, 0, 0, 0, 0) in the generated array.
+        boolean zeroRowFound = false;
+        for (int i = 0; i <= parkingPlace.length - 5; i++) {
+            if (parkingPlace[i] == 0 && parkingPlace[i + 1] == 0 && parkingPlace[i + 2] == 0 &&
+                    parkingPlace[i + 3] == 0 && parkingPlace[i + 4] == 0) {
+                zeroRowFound = true;
+                break;
+            }
+        }
+        assertTrue(zeroRowFound);
     }
 
     @Test
     void didCarMoveForwardTest() {
-        //car.con.setPosition(0);
-        car.MoveForward();
-        car.MoveForward();
-        car.MoveForward();
-        car.MoveForward();
-        car.MoveForward();
-        car.MoveForward();
+        // Test whether the car can move forward correctly.
 
+        for (int i = 0; i < 6; i++) {
+            car.MoveForward();
+        }
+        // Assert that the car's position is updated correctly and not parked.
         assertEquals(600, car.con.getPosition());
-        assertEquals(false, car.con.getSituation());
+        assertFalse(car.con.getSituation());
 
+
+    }
+
+    @Test
+    void carShouldMoveUntillStopTest() {
+        //Setting up variables for testing that it should keep move for 10½ lapse.
+        int teenAndAHalfLapse = (10 * 501) + 250;
+        int posMiddle = 25000;
+        for (int i = 0; i < teenAndAHalfLapse; i++) {
+            car.MoveForward();
+        }
+        assertEquals(posMiddle, car.con.getPosition());
+    }
+
+    @Test
+    void didTheCarMoveToEndOfStreetAndStartOverFromTheBeginningTest() {
+        // Set the car's position to the end of the street.
+        car.con.setPosition(50000);
+        // Assert that the car's position is set correctly to the end of the street.
+        assertEquals(50000, car.con.getPosition());
+
+
+        // Move the car forward, which should wrap it around to the beginning of the street.
+        car.MoveForward();
+        // Assert that the car's position is now at the beginning of the street (startOver).
+        assertEquals(0, car.con.getPosition());
+    }
+
+    @Test
+    void shouldNotMoveBackwardsIfAtStartTest() {
+        // Test so the car can't move out of the street backwards
+        car.MoveBackwards();
+        assertEquals(0, car.con.getPosition());
     }
 
     @Test
     void didCarMoveBackwardsTest() {
-        car.MoveForward();
-        car.MoveBackwards();
+        // Test whether the car can move backward correctly.
+        int pos5 = 500;
+        for (int i = 0; i < 6; i++) {
+            car.MoveForward();
+            if (i == 5)
+                car.MoveBackwards();
+        }
 
-        assertEquals(0, car.con.getPosition());
-        assertEquals(false, car.con.getSituation());
+        // Assert that the car's position is updated correctly and not parked.
+        assertEquals(pos5, car.con.getPosition());
+        assertFalse(car.con.getSituation());
 
 
     }
 
-
     @Test
     void parkCarTest() {
-        for (int i = 0; i < 50; i++) {
+        // Test parking the car repeatedly while moving forward.
+       /* for (int i = 0; i < 50; i++) {
             car.MoveForward();
             car.Park();
-        }
-        int result = car.isEmpty();
-        System.out.println(result);
-        //car.MoveForward();
-        //MAN KAN INTE PARKERA OM INTE SENSORERNA SÄGER ATT DET FINNS EN PLATS, HALLÅ
-        assertEquals(true, car.con.getSituation());
+        }*/
+
+        System.out.println(Arrays.toString(parkingPlace));
+
+        car.WhereIs();
+        car.Park();
+        // Assert that the car is not parked (isEmpty() should not indicate a parking space).
+        assertFalse(car.con.getSituation());
     }
 
     @Test
     void unParkTest() {
+        // Test if the car can be unparked.
         car.UnPark();
-        assertEquals(false, car.con.getSituation());
+        // Assert that the car is not parked.
+        assertFalse(car.con.getSituation());
     }
 
     @Test
     void whereIsCarTest() {
-        car.MoveForward();
-        car.MoveForward();
-        car.MoveForward();
+        // Test whether the WhereIs method returns car position and situation correctly.
+        int pos3 = 300;
+        for (int i = 0; i < 3; i++) {
+            car.MoveForward();
+        }
 
-
-        assertEquals(300, car.WhereIs().getPosition());
-        assertEquals(false, car.WhereIs().getSituation());
+        // Assert that the position and situation match the expected values.
+        assertEquals(pos3, car.WhereIs().getPosition());
+        assertFalse(car.WhereIs().getSituation());
 
     }
 
     @Test
-    void sensorDataNotNoisyTest() {
-        int[] cleanData = {190, 150, 120, 180, 200};
-        assertFalse(car.isNoisy(cleanData));// Clean data should return false
-    }
-    @Test
-    void sensorDataNoisyTest() {
+    void testIsNoisy() {
+        // Test if the isNoisy method correctly identifies noisy data.
+
         int[] noisyData = {20, 150, 10, 200, 115};
         assertTrue(car.isNoisy(noisyData)); // Noisy data should return true
     }
 
     @Test
-    void sensorDataNegativeTest() {
-        int[] cleanData = {0, 0, 0, 0, -1};
-        assertTrue(car.isNoisy(cleanData));// Noisy data should return true
-    }
-
-    @Test
-    void sensorDataTooBigTest() {
-        int[] cleanData = {200, 190, 201, 198, 189};
-        assertTrue(car.isNoisy(cleanData));// Noisy data should return true
+    void testIsNotNoisy() {
+        // Test if the isNoisy method correctly identifies clean data.
+        int[] cleanData = {190, 150, 120, 180, 200};
+        assertFalse(car.isNoisy(cleanData));// Clean data should return false
     }
 
 
-
     @Test
-    void isEmptyWithTwoWorkingSensorsTest() {
+    void testIsEmpty() {
+        // Test if the isEmpty method calculates the average distance correctly.
+        int result = car.isEmpty();
         int expected_result = 0;
 
-        for (int val : dummySens1) {
+        // Calculate the expected result based on dummy sensor data.
+        for (int val : dummyNoSens1) {
             expected_result += val;
         }
-        for (int val : dummySens2) {
+        for (int val : dummyNoSens2) {
             expected_result += val;
         }
-        //It the expected result should be the same as the average of the clean date
-        assertEquals(expected_result / 10, car.isEmpty());
+        // Assert that the result matches the expected average.
+        assertEquals(expected_result / 10, result);
     }
 
     @Test
-    void isEmptyWithFirstSensorBrokenTest() {
-        int[] brokenSensor = {200,10,200,30,150};
-        AutoParkingCar dummyCar = new AutoParkingCar(dummySens1,brokenSensor,new AutoParkingCar.context(0,false));
+    void aFullRunTest() {
+        car.MoveForward();
+        car.MoveForward();
+        car.MoveForward();
+        car.MoveForward();
+        car.MoveForward();
+        car.WhereIs();
+        car.Park();
 
-        int expected_result = 0;
 
-        for (int val : dummySens1) {
-            System.out.println(val);
-            expected_result += val;
-        }
-
-        //It the expected result should only take the average of the working sensor, being the second one
-        assertEquals(expected_result / 5, dummyCar.isEmpty());
-    }
-
-    @Test
-    void isEmptyWithSecondSensorBrokenTest() {
-        int[] brokenSensor = {200,10,50,100,150};
-        AutoParkingCar dummyCar = new AutoParkingCar(brokenSensor,dummySens2,new AutoParkingCar.context(0,false));
-
-        int expected_result = 0;
-
-        for (int val : dummySens2) {
-            System.out.println(val);
-            expected_result += val;
-        }
-
-        //It the expected result should only take the average of the working sensor, being the second one
-        assertEquals(expected_result / 5, dummyCar.isEmpty());
-    }
-
-    @Test
-    void isEmptyWithBothSensorsBrokenTest() {
-        int[] brokenSensorOne = {200,10,50,100,150};
-        int[] brokenSensorTwo = {500000,10,50,100,150};
-        AutoParkingCar dummyCar = new AutoParkingCar(brokenSensorOne,brokenSensorTwo,new AutoParkingCar.context(0,false));
-
-        //Faulty return value indicated with length -1
-        assertEquals(-1 , dummyCar.isEmpty());
     }
 
 }
