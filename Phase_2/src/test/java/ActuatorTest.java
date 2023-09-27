6f8392ad84d0da8d6a8d3a67b8f63d8795144537
simba <johanelfing@gmail.com>
2023-09-27 17:14:28 +0200

    Changes by Wilhelm

--- Phase_2/src/test/java/ActuatorTest.java
+++ Phase_2/src/test/java/ActuatorTest.java
@@ -1,14 +1,68 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ActuatorTest {
+
+    public Actuator actuator;
+    public AutoParkingCar.context context;
+
+    private int endOfTheStreet = 50000;
+    private int startOfStreet = 0;
+
+    private int oneMeter = 100;
+
     @BeforeEach
-    void setup(){
+    void setup() {
+        actuator = new Actuator();
+        context = new AutoParkingCar.context(startOfStreet, false);
+    }
+
+    @Test
+    void driveTest() {
+        int result = context.getPosition();
+        actuator.drive(context);
+        Assertions.assertEquals(context.getPosition(), result + oneMeter);
+    }
+
+    @Test
+    void reverseTest() {
+        context.setPosition(100);
+        int result = context.getPosition();
+        actuator.reverse(context);
+        Assertions.assertEquals(context.getPosition(), result - oneMeter);
+    }
+
+    @Test
+    void reverseStartOfStreet() {
+        int result = context.getPosition();
+        actuator.reverse(context);
+        Assertions.assertEquals(context.getPosition(), result);
+    }
+
+    @Test
+    void driveEndOfStreet() {
+        context.setPosition(endOfTheStreet);
+        int result = context.getPosition();
+        actuator.drive(context);
+        Assertions.assertNotEquals(context.getPosition(), result);
+    }
+
+    @Test
+    void driveWhenParked() {
+        context.setSituation(true);
+        actuator.drive(context);
+        Assertions.assertTrue(context.getSituation());
 
     }
 
     @Test
-    void testingActuator(){
+    void reverseWhenParked() {
+        context.setSituation(true);
+        actuator.reverse(context);
+        Assertions.assertTrue(context.getSituation());
 
     }
+
+
 }
