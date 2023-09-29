d9628919fe5f87b23a5d295e5b90aa43cd31ec68
Wilhelm von Hacht <77288567+kongstrupp@users.noreply.github.com>
2023-09-29 10:59:11 +0200

    johan act

--- Phase_2/src/main/java/Actuator.java
+++ Phase_2/src/main/java/Actuator.java
@@ -1,7 +1,8 @@
 public class Actuator {
-
     private int currentPosition;
     final private int oneMeter = 100;
+    final private int endOfTheStreet = 50000;
+    final private int startOfTheStreet = 0;
 
     public Actuator() {
         //Needs to have the same position as the car????
@@ -11,12 +12,15 @@ public class Actuator {
 
     public void drive(AutoParkingCar.context con) {
         // Simulate moving forward
-        con.setPosition(con.getPosition() + oneMeter);
+        if (con.getPosition() > endOfTheStreet) {
+            con.setPosition(startOfTheStreet);
+        }
+        con.setPosition(currentPosition + oneMeter);
     }
 
     public void reverse(AutoParkingCar.context con) {
         // Simulate moving backward
-        con.setPosition(con.getPosition()  - oneMeter);
+        con.setPosition(currentPosition - oneMeter);
     }
 
 }
